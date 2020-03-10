97499067b0823d6ee8aeae41f7566c47e8a0b3da
Ojo Oluwaseyi Shadrach <31080918+OS-the-Person@users.noreply.github.com>
2020-03-10 12:26:30 +0100

    update

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var textview = document.forms["myForm"]["textview"]
+
+function insertNum(num) {
+    textview.value = textview.value + num;
+}
+function insertOp(op) {
+    
+    textview.value += op;
+}
+
+function equal() {
+    var exp = textview.value;
+    if (exp) {
+        textview.value = eval(exp);
+    }
+}
+
+function clean() {
+    textview.value = " ";
+}
