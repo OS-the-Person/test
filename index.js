b7e8297d1bf06e15b6a6b25c4f9e4dbd4f35760e
Ojo Oluwaseyi Shadrach <31080918+OS-the-Person@users.noreply.github.com>
2020-03-11 10:40:55 +0100

    update

--- index.js
+++ index.js
@@ -1,23 +1,76 @@
 
-var textview = document.forms["myForm"]["textview"]
+var textview = document.forms["myForm"]["textview"];
 var lastItemDec = false;
+var valueArr = [];
 
 function insertVal(val) {
-    textview.value = textview.value + val;
+    if ("\+-*".includes(val)){  //if value is an operator
+        if (textview.value == "") return;
+        else {
+            let text;
+            text = textview.value;//add textbox items to array of items
+            valueArr = [];
+            valueArr.push(text);
+            
+            textview.value = "";//clear textbox
+            console.log("done");
+            
+            valueArr.push(val); //add operator to array
+            //textview.value = textview.value + val;
+            console.log(valueArr);
+        }
+    }   
+    let regexr = /\d/;
+    if (regexr.test(val)) {// if val is a digit
+        textview.value = textview.value + val;
+    }
+    if (val == "."){
+        if (textview.value.includes(".")) return;
+        else textview.value = textview.value + "."
+    }
 }
 
+
+/*
+/*["123.45", "/","3245.456"]
 function insertDec() {
-    if (textview.value[textview.value.length - 1] == ".") return;
-    else textview.value = textview.value + ".";
+ /*
+    if last item is a dec, or operator don't add
+    if the index of last dec is greater than index of last op don't add
+    if no op in string and there is a dec don't add
+
+    if (textview.value == "") return;
+    
+    if ("/*+.".includes(textview.value[textview.value.length - 1])) return;
+    
+    if (textview.value.lastIndexOf(".") > textview.value.lastIndexOf("/") || textview.value.lastIndexOf(".") > textview.value.lastIndexOf("*") || textview.value.lastIndexOf(".") > textview.value.lastIndexOf("+") || textview.value.lastIndexOf(".") > textview.value.lastIndexOf("-")) return;
+    
+    /*if (textview.value.includes())
+    
+    if (textview.value)
+    
+    if (textview.value.includes(".")){
+        if 
+    }
+
 }
 
 function insertOp(val) {
+    let text;
     if ("/*+".includes(textview.value[textview.value.length - 1])) return;
-    else textview.value = textview.value + val;
+    else {
+        text = textview.value;
+        valueArr.push(text);
+        
+        valueArr.push(val);
+        textview.value = textview.value + val;
+    }
 }
+*/
 
 function equal() {
-    var exp = textview.value;
+    valueArr.push(textview.value)
+    var exp = valueArr.join("");
     if (exp) {
         textview.value = eval(exp);
     }
@@ -25,4 +78,5 @@ function equal() {
 
 function clean() {
     textview.value = "";
+    valueArr = [];
 }
